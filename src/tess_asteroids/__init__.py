b6a2d8bd504f57fce1b7622f17fad79337cbcf35
altuson <73840616+altuson@users.noreply.github.com>
2026-03-24 13:59:35 -0400

    Bump version

--- src/tess_asteroids/__init__.py
+++ src/tess_asteroids/__init__.py
@@ -44,7 +44,7 @@ default_bad_spoc_bits = [1, 2, 3, 4, 5, 6, 8, 10, 13, 15]
 # or negative raw flux, or cadence is in region of non-science data during sector 3.
 default_bad_lc_bits = [2, 4, 10, 14]
 
-__version__ = "1.5.0"
+__version__ = "1.5.1"
 __all__ = ["MovingTPF"]
 
 from .movingtpf import MovingTPF  # noqa: E402
